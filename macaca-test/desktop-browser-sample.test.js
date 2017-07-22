92b87e4271e84626a060990e5fe7b6cb99ae40a6
xudafeng <xudafeng@126.com>
2017-07-22 11:31:19 +0800

    support custom ua

--- macaca-test/desktop-browser-sample.test.js
+++ macaca-test/desktop-browser-sample.test.js
@@ -23,7 +23,8 @@ describe('macaca desktop sample', function() {
     return driver
       .init({
         platformName: 'desktop',
-        browserName: browser
+        browserName: browser,
+        userAgent: `Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0 Safari/537.36 Macaca Custom UserAgent`
       })
       .maximize()
       .setWindowSize(1280, 800);
--- macaca-test/pages/sample.js
+++ macaca-test/pages/sample.js
@@ -1,7 +1,9 @@
-document.getElementById('hover_text').onmouseover = function() {
+document.querySelector('#hover_text').onmouseover = function() {
   document.getElementById('hover_text').style.color = 'red';
 };
 
-document.getElementById('alert_button').onclick = function() {
+document.querySelector('#alert_button').onclick = function() {
   alert('this message is from alert');
 };
+
+document.querySelector('#userAgent').innerHTML = navigator.userAgent;
